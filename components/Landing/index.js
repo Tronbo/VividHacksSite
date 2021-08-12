e84f3e3307c4df5effc2a5c3bea4e2e97edf2dd3
Manish674 <manishchoudhary4905@gmail.com>
2021-08-12 20:35:49 +0530

    Srot of completed navbar started working on landing page

--- components/Landing/index.js
+++ components/Landing/index.js
@@ -1,7 +1,35 @@
+import styles from "./landing.module.css";
+import Svg from "../../public/logo.svg";
+
 const Landing = () => {
   return (
-    <div>
-
+    <div className={styles.container}>
+      <span className={styles.heading}>
+        <h1 className={styles.topHeading}>
+          Hacking for <br />
+          EVERYONE.
+        </h1>
+      </span>
+      <button className={styles.button}>Register Now</button>
+      <h3 className={styles.date}>October 12, 2021</h3>
+      <p>
+        A 60-Hour Student Lead Hackathon Designed For{" "}
+        <span className={styles.text}>Everybody.</span>
+      </p>
+      <div className={styles.horizontalRule}>
+        <div className={styles.line} />
+        <img
+          src={Svg.src}
+          alt="logo.svg"
+          style={{
+            width: "4.5rem",
+            marginRight: "0.5rem",
+          }}
+        />
+        <div className={styles.line} />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Landing;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/navbar";
+import Landing from "../components/Landing";
 
 export default function Home() {
   return (
-    <div>
-    
-    </div>
-  )
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
 }
